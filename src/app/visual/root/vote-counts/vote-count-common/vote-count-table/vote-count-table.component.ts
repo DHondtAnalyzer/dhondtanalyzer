a02bf933d6ef026703bd7052211ad30d569655aa
Sergio Garcia <sergio@garciparedes.me>
2017-01-04 01:18:00 +0100

    Minor changes

--- src/app/visual/root/vote-counts/vote-count-common/vote-count-table/vote-count-table.component.ts
+++ src/app/visual/root/vote-counts/vote-count-common/vote-count-table/vote-count-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AppRow} from "../shared/app-row";
+import {AppColumn} from "../shared/app-column";
 
 @Component({
   selector: 'app-vote-count-table',
@@ -7,221 +9,248 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoteCountTableComponent implements OnInit {
 
+  private _rows: any[];
+  private _columns: any[];
+  private _loadingIndicator: boolean;
+
   constructor() {
-    setTimeout(() => { this.loadingIndicator = false; }, 1500);
+    setTimeout(() => { this.loadingIndicator = false; }, 5000);
   }
 
   ngOnInit() {
+    this.rows = [
+      {
+        name: 'Austin',
+        gender: 'Male',
+        gender2: 'Male',
+        gender3: 'Male',
+        gender4: 'Male',
+        gender5: 'Male',
+        gender6: 'Male',
+        gender7: 'Male',
+        gender8: 'Male',
+        company: 'Swimlane',
+      },
+      {
+        name: 'Austin',
+        gender: 'Male',
+        gender2: 'Male',
+        gender3: 'Male',
+        gender4: 'Male',
+        gender5: 'Male',
+        gender6: 'Male',
+        gender7: 'Male',
+        gender8: 'Male',
+        company: 'Swimlane',
+      },
+      {
+        name: 'Austin',
+        gender: 'Male',
+        gender2: 'Male',
+        gender3: 'Male',
+        gender4: 'Male',
+        gender5: 'Male',
+        gender6: 'Male',
+        gender7: 'Male',
+        gender8: 'Male',
+        company: 'Swimlane',
+      },
+      {
+        name: 'Austin',
+        gender: 'Male',
+        gender2: 'Male',
+        gender3: 'Male',
+        gender4: 'Male',
+        gender5: 'Male',
+        gender6: 'Male',
+        gender7: 'Male',
+        gender8: 'Male',
+        company: 'Swimlane',
+      },
+      {
+        name: 'Austin',
+        gender: 'Male',
+        gender2: 'Male',
+        gender3: 'Male',
+        gender4: 'Male',
+        gender5: 'Male',
+        gender6: 'Male',
+        gender7: 'Male',
+        gender8: 'Male',
+        company: 'Swimlane',
+      },
+      {
+        name: 'Austin',
+        gender: 'Male',
+        gender2: 'Male',
+        gender3: 'Male',
+        gender4: 'Male',
+        gender5: 'Male',
+        gender6: 'Male',
+        gender7: 'Male',
+        gender8: 'Male',
+        company: 'Swimlane',
+      },
+      {
+        name: 'Austin',
+        gender: 'Male',
+        gender2: 'Male',
+        gender3: 'Male',
+        gender4: 'Male',
+        gender5: 'Male',
+        gender6: 'Male',
+        gender7: 'Male',
+        gender8: 'Male',
+        company: 'Swimlane',
+      },
+      {
+        name: 'Austin',
+        gender: 'Male',
+        gender2: 'Male',
+        gender3: 'Male',
+        gender4: 'Male',
+        gender5: 'Male',
+        gender6: 'Male',
+        gender7: 'Male',
+        gender8: 'Male',
+        company: 'Swimlane',
+      },
+      {
+        name: 'Austin',
+        gender: 'Male',
+        gender2: 'Male',
+        gender3: 'Male',
+        gender4: 'Male',
+        gender5: 'Male',
+        gender6: 'Male',
+        gender7: 'Male',
+        gender8: 'Male',
+        company: 'Swimlane',
+      },
+      {
+        name: 'Austin',
+        gender: 'Male',
+        gender2: 'Male',
+        gender3: 'Male',
+        gender4: 'Male',
+        gender5: 'Male',
+        gender6: 'Male',
+        gender7: 'Male',
+        gender8: 'Male',
+        company: 'Swimlane',
+      },
+      {
+        name: 'Austin',
+        gender: 'Male',
+        gender2: 'Male',
+        gender3: 'Male',
+        gender4: 'Male',
+        gender5: 'Male',
+        gender6: 'Male',
+        gender7: 'Male',
+        gender8: 'Male',
+        company: 'Swimlane',
+      },
+      {
+        name: 'Austin',
+        gender: 'Male',
+        gender2: 'Male',
+        gender3: 'Male',
+        gender4: 'Male',
+        gender5: 'Male',
+        gender6: 'Male',
+        gender7: 'Male',
+        gender8: 'Male',
+        company: 'Swimlane',
+      },
+      {
+        name: 'Austin',
+        gender: 'Male',
+        gender2: 'Male',
+        gender3: 'Male',
+        gender4: 'Male',
+        gender5: 'Male',
+        gender6: 'Male',
+        gender7: 'Male',
+        gender8: 'Male',
+        company: 'Swimlane',
+      },
+      {
+        name: 'Austin',
+        gender: 'Male',
+        gender2: 'Male',
+        gender3: 'Male',
+        gender4: 'Male',
+        gender5: 'Male',
+        gender6: 'Male',
+        gender7: 'Male',
+        gender8: 'Male',
+        company: 'Swimlane',
+      },
+      {
+        name: 'Austin',
+        gender: 'Male',
+        gender2: 'Male',
+        gender3: 'Male',
+        gender4: 'Male',
+        gender5: 'Male',
+        gender6: 'Male',
+        gender7: 'Male',
+        gender8: 'Male',
+        company: 'Swimlane',
+      },{
+        name: 'Austin',
+        gender: 'Male',
+        gender2: 'Male',
+        gender3: 'Male',
+        gender4: 'Male',
+        gender5: 'Male',
+        gender6: 'Male',
+        gender7: 'Male',
+        gender8: 'Male',
+        company: 'Swimlane',
+      },
+
+
+    ];
+    this.columns = [
+      { prop: 'name' },
+      { name: 'Gender' },
+      { name: 'Gender2' },
+      { name: 'Gender3' },
+      { name: 'Gender4' },
+      { name: 'Gender5' },
+      { name: 'Gender6' },
+      { name: 'Gender7' },
+      { name: 'Gender8' },
+      { name: 'Company' }
+    ];
+    this.loadingIndicator = true;
   }
 
-  rows = [
-    {
-      name: 'Austin',
-      gender: 'Male',
-      gender2: 'Male',
-      gender3: 'Male',
-      gender4: 'Male',
-      gender5: 'Male',
-      gender6: 'Male',
-      gender7: 'Male',
-      gender8: 'Male',
-      company: 'Swimlane',
-    },
-    {
-      name: 'Austin',
-      gender: 'Male',
-      gender2: 'Male',
-      gender3: 'Male',
-      gender4: 'Male',
-      gender5: 'Male',
-      gender6: 'Male',
-      gender7: 'Male',
-      gender8: 'Male',
-      company: 'Swimlane',
-    },
-    {
-      name: 'Austin',
-      gender: 'Male',
-      gender2: 'Male',
-      gender3: 'Male',
-      gender4: 'Male',
-      gender5: 'Male',
-      gender6: 'Male',
-      gender7: 'Male',
-      gender8: 'Male',
-      company: 'Swimlane',
-    },
-    {
-      name: 'Austin',
-      gender: 'Male',
-      gender2: 'Male',
-      gender3: 'Male',
-      gender4: 'Male',
-      gender5: 'Male',
-      gender6: 'Male',
-      gender7: 'Male',
-      gender8: 'Male',
-      company: 'Swimlane',
-    },
-    {
-      name: 'Austin',
-      gender: 'Male',
-      gender2: 'Male',
-      gender3: 'Male',
-      gender4: 'Male',
-      gender5: 'Male',
-      gender6: 'Male',
-      gender7: 'Male',
-      gender8: 'Male',
-      company: 'Swimlane',
-    },
-    {
-      name: 'Austin',
-      gender: 'Male',
-      gender2: 'Male',
-      gender3: 'Male',
-      gender4: 'Male',
-      gender5: 'Male',
-      gender6: 'Male',
-      gender7: 'Male',
-      gender8: 'Male',
-      company: 'Swimlane',
-    },
-    {
-      name: 'Austin',
-      gender: 'Male',
-      gender2: 'Male',
-      gender3: 'Male',
-      gender4: 'Male',
-      gender5: 'Male',
-      gender6: 'Male',
-      gender7: 'Male',
-      gender8: 'Male',
-      company: 'Swimlane',
-    },
-    {
-      name: 'Austin',
-      gender: 'Male',
-      gender2: 'Male',
-      gender3: 'Male',
-      gender4: 'Male',
-      gender5: 'Male',
-      gender6: 'Male',
-      gender7: 'Male',
-      gender8: 'Male',
-      company: 'Swimlane',
-    },
-    {
-      name: 'Austin',
-      gender: 'Male',
-      gender2: 'Male',
-      gender3: 'Male',
-      gender4: 'Male',
-      gender5: 'Male',
-      gender6: 'Male',
-      gender7: 'Male',
-      gender8: 'Male',
-      company: 'Swimlane',
-    },
-    {
-      name: 'Austin',
-      gender: 'Male',
-      gender2: 'Male',
-      gender3: 'Male',
-      gender4: 'Male',
-      gender5: 'Male',
-      gender6: 'Male',
-      gender7: 'Male',
-      gender8: 'Male',
-      company: 'Swimlane',
-    },
-    {
-      name: 'Austin',
-      gender: 'Male',
-      gender2: 'Male',
-      gender3: 'Male',
-      gender4: 'Male',
-      gender5: 'Male',
-      gender6: 'Male',
-      gender7: 'Male',
-      gender8: 'Male',
-      company: 'Swimlane',
-    },
-    {
-      name: 'Austin',
-      gender: 'Male',
-      gender2: 'Male',
-      gender3: 'Male',
-      gender4: 'Male',
-      gender5: 'Male',
-      gender6: 'Male',
-      gender7: 'Male',
-      gender8: 'Male',
-      company: 'Swimlane',
-    },
-    {
-      name: 'Austin',
-      gender: 'Male',
-      gender2: 'Male',
-      gender3: 'Male',
-      gender4: 'Male',
-      gender5: 'Male',
-      gender6: 'Male',
-      gender7: 'Male',
-      gender8: 'Male',
-      company: 'Swimlane',
-    },
-    {
-      name: 'Austin',
-      gender: 'Male',
-      gender2: 'Male',
-      gender3: 'Male',
-      gender4: 'Male',
-      gender5: 'Male',
-      gender6: 'Male',
-      gender7: 'Male',
-      gender8: 'Male',
-      company: 'Swimlane',
-    },
-    {
-      name: 'Austin',
-      gender: 'Male',
-      gender2: 'Male',
-      gender3: 'Male',
-      gender4: 'Male',
-      gender5: 'Male',
-      gender6: 'Male',
-      gender7: 'Male',
-      gender8: 'Male',
-      company: 'Swimlane',
-    },{
-      name: 'Austin',
-      gender: 'Male',
-      gender2: 'Male',
-      gender3: 'Male',
-      gender4: 'Male',
-      gender5: 'Male',
-      gender6: 'Male',
-      gender7: 'Male',
-      gender8: 'Male',
-      company: 'Swimlane',
-    },
 
+  get rows(): any[] {
+    return this._rows;
+  }
 
-  ];
-  columns = [
-    { prnameop: 'name' },
-    { name: 'Gender' },
-    { name: 'Gender2' },
-    { name: 'Gender3' },
-    { name: 'Gender4' },
-    { name: 'Gender5' },
-    { name: 'Gender6' },
-    { name: 'Gender7' },
-    { name: 'Gender8' },
-    { name: 'Company' }
-  ];
-  loadingIndicator: boolean = true;
+  set rows(value: any[]) {
+    this._rows = value;
+  }
+  get columns(): any[] {
+    return this._columns;
+  }
+
+  set columns(value: any[]) {
+    this._columns = value;
+  }
+
+  get loadingIndicator(): boolean {
+    return this._loadingIndicator;
+  }
+
+  set loadingIndicator(value: boolean) {
+    this._loadingIndicator = value;
+  }
 
 
 }
