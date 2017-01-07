1087ca339be3cbb7ea2b91b0fc37b9967397a644
Sergio Garcia <sergio@garciparedes.me>
2017-01-07 23:50:44 +0100

    Changes in vote-count

--- src/app/dao/model/vote-count.ts
+++ src/app/dao/model/vote-count.ts
@@ -5,6 +5,16 @@
 import {VoteType} from "./vote-type";
 import {District} from "./district";
 import {Party} from "./party";
+import {ModelRaw} from "./model";
+import {AppObjectObservable} from "../app-object-observable";
+
+
+export interface VoteCountRaw extends ModelRaw {
+  count: number;
+  type: VoteType;
+  district: any;
+  party: any;
+}
 
 
 export class VoteCount {
@@ -14,23 +24,34 @@ export class VoteCount {
     count: number;
     type: VoteType;
 
-    district: District;
-    party: Party;
+  district: AppObjectObservable<District>;
+  party: AppObjectObservable<Party>;
 
 
-    public static newInstance(district: District, type: VoteType,
-                              count: number, party?:Party): VoteCount {
-        let voteCount = new VoteCount(district, type, count, party);
+  public static newInstance(count: number = 0, type: VoteType = VoteType.VALID,
+                            district: AppObjectObservable<District>,
+                            party?: AppObjectObservable<Party>): VoteCount {
 
-        district.voteCountList.push(voteCount);
+    return new VoteCount(null, count, type, district, party);
+  }
 
-        return voteCount;
-    }
+  public static fromRaw(raw: VoteCountRaw){
+    return new VoteCount(
+      raw.$key,
+      raw.count,
+      raw.type,
+      raw.district,
+      raw.party
+    );
+  }
 
-    constructor(district: District, type: VoteType, count: number, party?:Party){
-        this.district = district;
-        this.type =  type;
-        this.count = count;
-        this.party = party;
-    }
+  constructor(key: string, count: number, type: VoteType,
+              district: AppObjectObservable<District>,
+              party?: AppObjectObservable<Party>) {
+    this.id = key;
+    this.district = district;
+    this.type = type;
+    this.count = count;
+    this.party = party;
+  }
 }
